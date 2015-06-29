8e90b9ded04338c20dc2de574c7653089a124896
John Kennedy <jkennedy@fitbit.com>
2015-06-29 11:50:34 -0700

    new paths for styleguide

--- app/controllers/kss/application_controller.rb
+++ app/controllers/kss/application_controller.rb
@@ -4,14 +4,34 @@ module Kss
 
     private
 
-    def styleguide
-      @styleguide ||= Kss::Parser.new(File.expand_path('app/assets/stylesheets', Rails.root))
+    def text
+      @text ||= Kss::Parser.new(File.expand_path('app/assets/stylesheets', Rails.root))
     end
-    helper_method :styleguide
+    helper_method :text
 
     def buttons
       @buttons ||= Kss::Parser.new(File.expand_path('app/assets/stylesheets', Rails.root))
     end
     helper_method :buttons
+
+    def modal
+      @modal ||= Kss::Parser.new(File.expand_path('app/assets/stylesheets', Rails.root))
+    end
+    helper_method :modal
+
+    def forms
+      @forms ||= Kss::Parser.new(File.expand_path('app/assets/stylesheets', Rails.root))
+    end
+    helper_method :forms
+
+    def hints
+      @hints ||= Kss::Parser.new(File.expand_path('app/assets/stylesheets', Rails.root))
+    end
+    helper_method :hints
+
+    def toggle_box
+      @toggle_box ||= Kss::Parser.new(File.expand_path('app/assets/stylesheets', Rails.root))
+    end
+    helper_method :toggle_box
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Kss::Engine.routes.draw do
-  get '/styleguide' => 'home#styleguide'
+  get '/text' => 'home#text'
   get '/buttons' => 'home#buttons'
+  get '/modal' => 'home#modal'
+  get '/forms' => 'home#forms'
+  get '/hints' => 'home#hints'
+  get '/toggle-box' => 'home#toggle_box', :as => 'toggle_box'
 
   root :to => 'home#index'
 end
